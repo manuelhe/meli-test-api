d89263efa4bda10184fc63e7bbeda7976c917af6
Manuel Herrera <334509+manuelhe@users.noreply.github.com>
2021-01-09 13:49:31 -0500

    feat: add general config constants

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  API_BASE_PATH: 'https://api.mercadolibre.com/',
+  API_SEARCH_PATH: 'sites/MLA/search',
+  API_ITEMS_PATH: 'items/',
+  ITEMS_PER_PAGE: 4,
+  API_AUTHOR_INFO: { name: 'Manuel', lastname: 'Herrera' },
+};
