8fdd9e1dc6fc8fd6bae71e48b667563ff1ef3ce1
Manuel Herrera <334509+manuelhe@users.noreply.github.com>
2021-01-09 13:49:04 -0500

    chore: fix linting issues

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var users = require('./routes/users');
+const search = require('./routes/search');
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use('/api/v1/users', users);
+app.use('/api/v1/search', search);
 
 module.exports = app;
