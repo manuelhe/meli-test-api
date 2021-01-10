0cede3b0cb2f7b3a38c26dd017d21a980dc6b33c
Manuel Herrera <334509+manuelhe@users.noreply.github.com>
2021-01-09 22:18:40 -0500

    feat: modify base component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Switch>
+          <Route exact path='/items' component={ListItems}/>
+          <Route path='/items/:id' component={ItemDetails}/>
+      </Switch>
     </div>
   );
 }
+
+const ListItems = () => {
+  return (
+    <h1>ListItems</h1>
+  );
+};
+
+const ItemDetails = () => {
+  return (
+    <h1>ItemDetails</h1>
+  );
+};
 
 export default App;
