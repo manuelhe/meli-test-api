0cb1154b9f8648f83c6981cd8a539dc663670fd3
Manuel Herrera <334509+manuelhe@users.noreply.github.com>
2021-01-10 20:13:02 -0500

    feat: add recoilroot to main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
 import { Route, Switch } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import Header from './components/Header/Header';
 import ItemsDetails from './containers/ItemDetails/ItemDetails';
 import ListItems from './containers/ListItems/ListItems';
 
-const categories = [
-  'Electrónica Audio y Video',
-  'IPod',
-  'Reproductores',
-  'IPod Touch',
-  '32 GB',
-];
-
 function App() {
   return (
-    <div className="App">
-      <Header categories={categories}/>
-      <Switch>
-          <Route exact path='/items' component={ListItems}/>
-          <Route path='/items/:id' component={ItemsDetails}/>
-      </Switch>
-    </div>
+    <RecoilRoot>
+        <div className="App">
+          <Header/>
+          <Switch>
+              <Route exact path='/items' component={ListItems}/>
+              <Route path='/items/:id' component={ItemsDetails}/>
+          </Switch>
+        </div>
+    </RecoilRoot>
   );
 }
 
